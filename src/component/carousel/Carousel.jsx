568afa71653efe404476302bafe64e1ef2b3bb22
Traders of Africa <tradersofafrica@Ujus-MacBook-Pro-2.local>
2022-04-29 13:53:36 +0100

    added more on the landing page

---
+++ src/component/carousel/Carousel.jsx
@@ -0,0 +1,224 @@
+import React from "react";
+import farmer from "../../photos/farm.jpg";
+import mowing from "../../photos/tractor.png";
+import maiz from "../../photos/maize.png";
+import egus from "../../photos/egusi.png";
+import cash from "../../photos/cashew.png";
+import air from "../../photos/airbnb.png";
+import hub from "../../photos/hubspot.png";
+import micro from "../../photos/microsoft.png";
+import goog from "../../photos/google.png";
+import wal from "../../photos/walmart.png";
+import fed from '../../photos/fedex.png'
+import man from '../../photos/manspic.png'
+import { Link } from "react-router-dom";
+
+const Carousel = () => {
+  return (
+    <div>
+      <div
+        id="carouselExampleCaptions"
+        className="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={farmer} className="d-block w-100" alt="grainMap" />
+          </div>
+          <div className="carousel-item">
+            <img src={farmer} className="d-block w-100" alt="grainMap" />
+          </div>
+          <div className="carousel-item">
+            <img src={farmer} className="d-block w-100" alt="grainMap" />
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+
+      <div className="d-flex justify-content-between mt-5 px-5">
+        <div>
+          <img src={mowing} alt="tractor" />
+        </div>
+        <div className="py-5">
+          <h3 className="text-center px-5">Creating Visibility and Access</h3>
+          <p className="text-center px-5 tractortexts">
+            Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam
+            no suscipit quaerendum. At nam minimum ponderum. Est audiam animal
+            molestiae te. Ex duo eripuit mentitum Ex duo eirput mentitum. Et has
+            minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no
+            suscipit quaerendum. At nam minimum ponderum. Est audiam animal
+            molestiae te. Ex duo eripuit mentitum.
+            <br />
+            Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam
+            no suscipit quaerendum. At nam minimum ponderum. Est audiam animal
+            molestiae te. Ex duo eripuit mentitum.
+          </p>
+        </div>
+      </div>
+      <div className="mt-5">
+        <h3 className="products">Our Products</h3>
+        
+        <p className="product">
+          You can buy our African Sourced and manufactured products from the
+          comfort of your home
+        </p>
+      </div>
+      <div className="row mx-auto px-0">
+        <div className="col-4">
+        <img src={egus} alt="egusi" className=""/>
+        </div>
+        <div className="col-4">
+        <img src={maiz} alt="maize" className=""/>
+        </div>
+        <div className="col-4">
+        <img src={cash} alt="cashew" className=""/>
+        </div>
+
+      </div>
+      <div className="container d-flex pl-2">
+        <img src={egus} alt="egusi" className="cereals"/>
+        <img src={maiz} alt="maize" className="cereals"/>
+        <img src={cash} alt="cashew" className="cereal"/>
+      </div>
+
+      <div class="container event-container">
+            <div class="col-12 col-md-12 col-sm-1">
+              
+            </div>
+            <div class="event-img-wrapper">
+              
+            <div class="box a">a
+            <img src={egus} alt="egusi" className=""/>
+            </div>
+            <div class="box b">b
+            <img src={maiz} alt="maize" className=""/>
+            </div>
+            <div class="box c">c
+                <img src="../assets/images/ahhann-2@2x.png" align="left"/>
+            </div>
+            <div class="box d">d
+            <img src={cash} alt="cashew" className="cereal"/>
+            </div>
+            </div>
+            </div>
+
+      <div className="d-flex">
+        <div className="">
+          <div className="text-center">
+        <i className="fa-solid fa-business-time text-center"></i>
+        </div>
+        <h3 className="psourcing text-center">Product Sourcing</h3>
+        <p className="sourcing px-5">
+          SourcePro is a unit of TOFA that works directly with our
+          online/offline to ensure that buys specifications are met. The
+          SourcePro team handles products like wood, agro-commodities, textile
+          and finished products, all sourced from various African countries and
+          shipped to buyers within and outside Africa.<br/>
+          <Link to>Get Started</Link>
+        </p>
+        
+        </div>
+        <div>
+        <div className="text-center">
+        <i className="fa-solid fa-business-time text-center"></i>
+        </div>
+        <h3 className="tfinance text-center">Trade Finance</h3>
+        <p className="finance px-5">
+          Africa Trade Invest (ATI) is a Pan African digital trade and
+          investment platform. ATI is dedicated solely towards fundinf and
+          inversting in African trade, where investors can erarn a share of pr
+          adequate funding and execution to create a sustainable business,
+          yielding great and stable returns.{" "}<br/>
+          <Link to>Get Started</Link>
+        </p>
+        </div>
+      </div>
+      <div className="partners">
+        <h5 className="text-center mb-5">Our Partners</h5>
+        <div className="row mx-auto mb-5 partner">
+
+          <div className="col-2">
+          <img src={air} alt='airbnb logo' />
+          </div>
+          <div className="col-2">
+          <img src={hub} alt='hubspot logo' />
+          </div>
+          <div className="col-2">
+          <img src={goog} alt='google logo' />
+          </div>
+          <div className="col-2">
+          <img src={micro} alt='microsoft logo' />
+          </div>
+          <div className="col-2">
+          <img src={wal} alt='walmartlogo' />
+          </div>
+          <div className="col-2">
+          <img src={fed} alt='fedex logo' />
+          </div>
+        </div>
+        <div>
+          <h5 className="text-center my-5">Our Stories</h5>
+          <div className="row mx-auto mb-5 story">
+          <div className="col-4">
+          <img src={man} alt='man'/>
+          </div>
+          <div className="col-4">
+          <img src={man} alt='man'/>
+          </div>
+          <div className="col-4">
+          <img src={man} alt='man'/>
+          </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,36 +1,58 @@
-import React from 'react';
-import tofa from '../../photos/tofaLogo.png'
-import '../../index.css';
+import React from "react";
+import tofa from "../../photos/logolanding.png";
+import "../../index.css";
+import Carousel from "../../component/carousel/Carousel";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
-    return (
-        <div className='container-fluid bgbody'>
-            <div className='menuitems mt-2'>
-                <img src={tofa} alt='tofalogo'/>
-                <ul className='menuitems mx-auto d-flex justify-content-evenly'>
-                    <li>Trade Finance</li>
-                    <li>SourcePro</li>
-                    <li>Academy</li>
-                    <li>Support</li>
-                </ul>
-                <button className='btn btncolor'>MarketPlace</button>
-            </div>
-            <div>MarketPlace</div>
+  return (
+    <div className="container-fluid">
+        <div className="bgbody">
+      <Carousel />
+      </div>
 
-            <div className='arrange'>
-            <p>Small Business Communities, Big Market Opportunities</p>
-            <p>Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, am no suscipit quaerendum. At nam minimum </p>
-            </div>
-            
-            <div className='flex mx-auto'>
-            <button className='btn btncolor text-light'>Go to Market</button>
-            <button className='btn text-light'>Learn more</button>
-            
-            </div>
-
-
+      <div className="content">
+        <div className="menuitems">
+          <img src={tofa} alt="tofalogo" className="mt-3" />
+          <ul className="menuitems mx-auto d-flex justify-content-evenly text-light mt-3">
+            <Link to>
+              <li className="px-5 text-light">Trade Finance</li>
+            </Link>
+            <Link to>
+              <li className="px-5 text-light">SourcePro</li>
+            </Link>
+            <Link to>
+              <li className="px-5 text-light">Academy</li>
+            </Link>
+            <Link to>
+              <li className="px-5 text-light">Support</li>
+            </Link>
+          </ul>
+          <button className="btn btncolor mt-3">MarketPlace</button>
         </div>
-    )
-}
+
+        <div className="textss">
+          <div className="text-center text-light px-10">
+            <p className="descrip">
+              Small Business Communities,
+              <br /> Big Market Opportunities
+            </p>
+            <p>
+              Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad,
+              <br /> am no suscipit quaerendum. At nam minimum{" "}
+            </p>
+          </div>
+
+          <div className="flex mx-auto text-center">
+            <button className="btn btncolor text-light mx-4">
+              Go to Market
+            </button>
+            <button className="btn btnlight text-light">Learn more</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Landing;
