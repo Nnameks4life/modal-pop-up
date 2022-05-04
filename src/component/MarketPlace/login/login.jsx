91dff232ea4993ecbf18c5259d82c2ea09ee2967
Erhun Abbe <erhun@tradersoofafrica.com>
2022-05-04 09:23:22 +0100

    login page done

--- src/component/MarketPlace/login/login.jsx
+++ src/component/MarketPlace/login/login.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import grainMap from "../../../photos/grainMap.jpeg";
 import { useState } from "react";
-import facebook from "../../../photos/facebook 1.png";
-
+import "../login/login.css";
+import facebookImg from "../../../photos/fb.svg";
+import googleImg from "../../../photos/gl.svg";
+import backIcon from "../../../photos/back-icon.svg";
 
 export default function Login() {
   const [loginDetail, setLoginDetail] = useState({
@@ -17,139 +19,95 @@ export default function Login() {
     setLoginDetail({ ...loginDetail, [e.target.name]: [e.target.value] });
   };
   return (
-    <section className='' >
-        <div>
-          <img src={grainMap} alt="a map made with crops" />
+    <section id="user-auth">
+      <div className="row g-0">
+        <div className="col-lg-6 d-none d-lg-block">
+          <div className="user-auth-lhs"></div>
         </div>
-        <div className='w-50 mt-4'>
-          <div className=''>
-            <h5>Back</h5>
-            <p>
-              Don't have an account?{" "}
-              <a href="/register">
-                SignIn
-              </a>
-            </p>
-          </div>
-          <div>
-            <h4>Welcome back</h4>
-          </div>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label
-                htmlFor="email"
-              >
-                Email address
-              </label>
-              <input
-                
-                type="email"
-                name="email"
-                value={loginDetail.email}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="mt-7">
-              <label
-                htmlFor="password"
-                
-              >
-                Password
-              </label>
-              <input
-               
-                type="password"
-                name="password"
-                value={loginDetail.password}
-                onChange={handleChange}
-              />
-            </div>
-            <div>
-              <div >
-                <input
-                  id="remember_me"
-                  type="checkbox"
-                 
-                />
-                <label
-                  htmlFor="remember_me"
-                  
-                >
-                 
-                  Remember me
-                </label>
-              </div>
-              <div>
-              <a href="/forgotpassword">
-                Forgot your password?{" "}
-              </a>
+        <div className="col-lg-6">
+          <div className="user-auth-rhs">
+            <div className="user-nav">
+              <div className="row">
+                <div className="col-lg-6 col-3">
+                  <p className="back-txt">
+                    <img src={backIcon} />
+                    <a href="#">Back</a>
+                  </p>
+                </div>
+                <div className="col-lg-6 col-9">
+                  <p className="auth-info-txt">
+                    Don’t have an account?
+                    <span>
+                      <a href="#">SignUp</a>
+                    </span>
+                  </p>
+                </div>
               </div>
             </div>
-            <div className="mt-6 ">
-              <button className="w-full text-primary-white inline-flex items-center justify-center px-4 py-3 bg-primary-orange border-transparent rounded-md font-semibold capitalize hover:bg-red-700 active:bg-red-700 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200 disabled:opacity-25 transition">
-                Login
-              </button>
-            </div>
-          </form>
-          <div className="">
-            <div className="">
-              <hr className="" />
-              <p className="">
+            <div className="user-auth-wrap">
+              <h2>Welcome back</h2>
+              <form className="auth-form">
+                <div className="mb-3">
+                  <label htmlFor="exampleInputEmail1" className="form-label">
+                    Email address
+                  </label>
+                  <input
+                    type="email"
+                    className="form-control input"
+                    id="exampleInputEmail1"
+                    value={loginDetail.email}
+                    onChange={handleChange}
+                    placeholder="Enter email address..."
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="exampleInputPassword1" className="form-label">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control input"
+                    id="exampleInputPassword1"
+                    value={loginDetail.password}
+                    onChange={handleChange}
+                    placeholder="Enter password..."
+                  />
+                </div>
+                <div className="mb-3 form-check">
+                  <input
+                    type="checkbox"
+                    className="form-check-input"
+                    id="exampleCheck1"
+                  />
+                  <label className="form-check-label" htmlFor="exampleCheck1">
+                    Remember me
+                  </label>
+                </div>
+                <button type="submit" className="btn btn-danger button">
+                  Login
+                </button>
+              </form>
+              <p className="divider">
+                <span>Or</span>
               </p>
-              <hr className=""></hr>
+              <div className="social-btns">
+                <button className="btn btn-white button">
+                  <span>
+                    <img src={facebookImg} />
+                  </span>
+                  Sign in with Google
+                </button>
+                <button className="btn btn-white button">
+                  <span>
+                    <img src={googleImg} />
+                  </span>
+                  Sign in with Facebook
+                </button>
+              </div>
             </div>
           </div>
-          <div className="">
-            <button
-              aria-label="Continue with google"
-              className=""
-            >
-              <div className="pl-2">
-                <svg
-                  width="50"
-                  height="20"
-                  viewBox="0 0 19 20"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M18.9892 10.1871C18.9892 9.36767 18.9246 8.76973 18.7847 8.14966H9.68848V11.848H15.0277C14.9201 12.767 14.3388 14.1512 13.047 15.0812L13.0289 15.205L15.905 17.4969L16.1042 17.5173C17.9342 15.7789 18.9892 13.221 18.9892 10.1871Z"
-                    fill="#4285F4"
-                  />
-                  <path
-                    d="M9.68813 19.9314C12.3039 19.9314 14.4999 19.0455 16.1039 17.5174L13.0467 15.0813C12.2286 15.6682 11.1306 16.0779 9.68813 16.0779C7.12612 16.0779 4.95165 14.3395 4.17651 11.9366L4.06289 11.9465L1.07231 14.3273L1.0332 14.4391C2.62638 17.6946 5.89889 19.9314 9.68813 19.9314Z"
-                    fill="#34A853"
-                  />
-                  <path
-                    d="M4.17667 11.9366C3.97215 11.3165 3.85378 10.6521 3.85378 9.96562C3.85378 9.27905 3.97215 8.6147 4.16591 7.99463L4.1605 7.86257L1.13246 5.44363L1.03339 5.49211C0.37677 6.84302 0 8.36005 0 9.96562C0 11.5712 0.37677 13.0881 1.03339 14.4391L4.17667 11.9366Z"
-                    fill="#FBBC05"
-                  />
-                  <path
-                    d="M9.68807 3.85336C11.5073 3.85336 12.7344 4.66168 13.4342 5.33718L16.1684 2.59107C14.4892 0.985496 12.3039 0 9.68807 0C5.89885 0 2.62637 2.23672 1.0332 5.49214L4.16573 7.99466C4.95162 5.59183 7.12608 3.85336 9.68807 3.85336Z"
-                    fill="#EB4335"
-                  />
-                </svg>
-              </div>
-              <div className="pl-8">
-                <p className="">Sign in with Google</p>
-              </div>
-            </button>
-          </div>
-          <div className="mx-11">
-            <button
-              aria-label="Continue with google"
-              className=" "
-            >
-              <div className="">
-                <img src={facebook}/>
-              </div>
-              <div className="">
-                <p className="">Sign in with Facebook</p>
-              </div>
-            </button>
-          </div>
         </div>
-     
+      </div>
     </section>
   );
 }
