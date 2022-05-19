493b90e16156bb68cdf3b5600b8dc030ed6d15d8
Emmanuel Ocansey <developer.ocansey@gmail.com>
2022-05-19 18:36:12 +0300

    app.js: resolve imports error

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from './component/MarketPlace/login/login'
-import Register from './component/MarketPlace/registration/register'
-import Buyer from './component/MarketPlace/registration/buyer'
-import Seller from './component/MarketPlace/registration/seller';
-import ForgotPassword from './component/MarketPlace/password/forgotPassword'
-import HomePage from './component/MarketPlace/homepage/homePage'
-import ViewProduct from './component/MarketPlace/homepage/viewProduct'
-import Rfq from './component/MarketPlace/order/rfq'
-import OrderStatus from './component/MarketPlace/order/orderStatus'
-import Landing from "./pages/landing/Landing";
-import Nav from "./component/nav/Nav";
-import OrderStatus from './component/MarketPlace/orderStatus'
-import ProdSourcing from "./pages/prodsourcing/ProdSourcing";
-import Register from './component/MarketPlace/register'
-import Rfq from './component/MarketPlace/rfq'
-import Seller from './component/MarketPlace/seller';
-import TofaAcademy from "./pages/tofaacademy/TofaAcademy";
+
 import AcademyForm from "./pages/academyform/AcademyForm";
 import AllBlogs from "./pages/allblogs/AllBlogs";
 import BlogDetails from "./pages/blogdetails/BlogDetails";
+import Buyer from './component/MarketPlace/registration/buyer'
+import ContactUs from './pages/contactus/ContactUs'
+import Faq from "./pages/faq/Faq"
+import ForgotPassword from './component/MarketPlace/password/forgotPassword'
+import HomePage from './component/MarketPlace/homepage/homePage'
+import Landing from "./pages/landing/Landing";
+import Login from './component/MarketPlace/login/login'
+import Nav from "./component/nav/Nav";
+import OrderStatus from './component/MarketPlace/order/orderStatus'
+import ProdSourcing from "./pages/prodsourcing/ProdSourcing";
+import Register from './component/MarketPlace/registration/register'
+import Rfq from './component/MarketPlace/order/rfq'
+import Seller from './component/MarketPlace/registration/seller';
+import TofaAcademy from "./pages/tofaacademy/TofaAcademy";
+import ViewProduct from './component/MarketPlace/homepage/viewProduct'
+
 function App() {
   return (
       <BrowserRouter>
